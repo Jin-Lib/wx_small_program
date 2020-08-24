12bc3eaede18170a4751ca13bb53a8fe0f5f47c1
Jin <512364579@qq.com>
2020-08-24 10:47:56 +0800

    feat: 增加新功能
    1 增加年龄select
    2 增加上拉加载

--- pages/mall/index.js
+++ pages/mall/index.js
@@ -4,13 +4,64 @@ const app = getApp()
 
 Page({
   data: {
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    recommendedAge: ['4岁', '5岁'],
+    recommendedAgeSelectIndex: 0,
+    recommendedList: [
+      {
+        logoSrc: "http://cdn.koalaxiezi.com/image1/product.jpg",
+        title: "世界地理之最",
+        desc: "9天提升科学探索欲望",
+        buyCount: '77098',
+        buyImages: [
+          "http://cdn.koalaxiezi.com/image1/product.jpg",
+          "http://cdn.koalaxiezi.com/image1/product.jpg",
+          "http://cdn.koalaxiezi.com/image1/product.jpg",
+          "http://cdn.koalaxiezi.com/image1/product.jpg",
+          "http://cdn.koalaxiezi.com/image1/product.jpg",
+        ]
+      }
+    ],
   },
   //事件处理函数
   bindViewTap: function() {
     wx.navigateTo({
       url: '../logs/logs'
     })
+  },
+  // 智能推荐 选择年龄
+  recommendedAgeSelect(event) {
+    this.setData({
+      recommendedAgeSelectIndex: event.detail.value
+    }, () => {
+      console.log('选择完成')
+    })
+  },
+  // 上拉加载
+  onReachBottom() {
+    const resultList = {
+      logoSrc: "http://cdn.koalaxiezi.com/image1/product.jpg",
+      title: "世界地理之最",
+      desc: "9天提升科学探索欲望",
+      buyCount: '77098',
+      buyImages: [
+        "http://cdn.koalaxiezi.com/image1/product.jpg",
+        "http://cdn.koalaxiezi.com/image1/product.jpg",
+        "http://cdn.koalaxiezi.com/image1/product.jpg",
+        "http://cdn.koalaxiezi.com/image1/product.jpg",
+        "http://cdn.koalaxiezi.com/image1/product.jpg",
+      ]
+    }
+    wx.showLoading({
+      title: '努力加载中',
+    })
+    setTimeout(() => {
+      this.setData({
+        recommendedList: this.data.recommendedList.concat(new Array(5).fill(resultList))
+      }, () => {
+        wx.hideLoading()
+      })
+    }, 2000)
   },
   onLoad: function () {
     
