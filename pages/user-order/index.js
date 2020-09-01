6a0aa9cf5156bb05926ce1ac27b8aa5d2a3978ee
LAPTOP-GQ2RS962\67433 <704714320@qq.com>
2020-09-01 22:41:31 +0800

    no message

--- pages/user-order/index.js
+++ pages/user-order/index.js
@@ -1,7 +1,58 @@
 //index.js
 
+
 Page({
   data: {
+    selected: true,
+    selected1: false,
+    selected2: false,
+    selected3: false,
+    selected4: false,
+  },
+  selected: function (e) {
+    this.setData({
+      selected1: false,
+      selected: true,
+      selected2: false,
+      selected3: false,
+      selected4: false,
+    });
+  },
+  selected1: function (e) {
+    this.setData({
+      selected: false,
+      selected1: true,
+      selected2: false,
+      selected3: false,
+      selected4: false,
+    });
+  },
+  selected2: function (e) {
+    this.setData({
+      selected: false,
+      selected1: false,
+      selected2: true,
+      selected3: false,
+      selected4: false,
+    });
+  },
+  selected3: function (e) {
+    this.setData({
+      selected: false,
+      selected1: false,
+      selected2: false,
+      selected3: true,
+      selected4: false,
+    });
+  },
+  selected4: function (e) {
+    this.setData({
+      selected: false,
+      selected1: false,
+      selected2: false,
+      selected3: false,
+      selected4: true,
+    });
   },
   onLoad: function () {
 
