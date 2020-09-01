5067d848407721458bf5c650665914b444c247d6
LAPTOP-GQ2RS962\67433 <704714320@qq.com>
2020-09-01 11:22:40 +0800

    用户信息

--- pages/money/index.js
+++ pages/money/index.js
@@ -14,6 +14,18 @@ Page({
   onLoad: function () {
     
   },
+  //点击累计奖学金收益
+  income: function (event) {
+    wx.navigateTo({
+      url: '/pages/user-bonus-income/index'
+    })
+  },
+  //点击提现
+  extract: function (event) {
+    wx.navigateTo({
+      url: '/pages/user-extract/index'
+    })
+  },
   /**
    * 生命周期函数--监听页面显示
    */
