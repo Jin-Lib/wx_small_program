2ac3bf368e55c990096427b2259db720577abff5
LAPTOP-GQ2RS962\67433 <704714320@qq.com>
2020-09-01 18:37:15 +0800

    优惠券完成

--- pages/user-coupons/index.js
+++ pages/user-coupons/index.js
@@ -1,7 +1,32 @@
 //index.js
 
+
 Page({
   data: {
+    selected: true,
+    selected1: false,
+    selected2: false,
+  },
+  selected: function (e) {
+    this.setData({
+      selected1: false,
+      selected: true,
+      selected2: false,
+    });
+  },
+  selected1: function (e) {
+    this.setData({
+      selected: false,
+      selected1: true,
+      selected2: false,
+    });
+  },
+  selected2: function (e) {
+    this.setData({
+      selected: false,
+      selected1: false,
+      selected2: true,
+    });
   },
   onLoad: function () {
 
