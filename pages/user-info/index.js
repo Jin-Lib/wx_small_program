ecda5259e92e884ffd22827254ae4e3ca8b242c0
LAPTOP-GQ2RS962\67433 <704714320@qq.com>
2020-09-03 20:23:17 +0800

    no message

--- pages/user-info/index.js
+++ pages/user-info/index.js
@@ -5,6 +5,48 @@ const API = require('../../config/api');
 
 Page({
   data: {
+    hidden: true, //头像弹窗
+    namehidden: true, //修改姓名弹窗
+    iphonehidden: true, //修改手机号弹窗
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    recommendedAge: ['4岁', '5岁'],
+    recommendedAgeSelectIndex: 0,
+    recommendedAgexb: ['0', '1'],
+    recommendedAgeSelectIndexxb: 0,
+  },
+  //头像弹窗按钮
+  clickImg: function (e) {
+    this.setData({
+      hidden: !this.data.hidden
+    })
+  },
+  //姓名弹窗
+  nameshow: function (e) {
+    this.setData({
+      namehidden: !this.data.namehidden
+    })
+  },
+  //手机号弹窗
+  iphoneshow: function (e) {
+    this.setData({
+      iphonehidden: !this.data.iphonehidden
+    })
+  },
+  // 选择生日
+  recommendedAgeSelect(event) {
+    this.setData({
+      recommendedAgeSelectIndex: event.detail.value
+    }, () => {
+      console.log('选择完成')
+    })
+  },
+  // 选择性别
+  recommendedAgeSelectxb(event) {
+    this.setData({
+      recommendedAgeSelectIndexxb: event.detail.value
+    }, () => {
+      console.log('选择完成')
+    })
   },
   onLoad: function () {
     this.getinfoData()
