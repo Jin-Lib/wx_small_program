e0316831363ba39c0255af3118cd47625545583f
LAPTOP-GQ2RS962\67433 <704714320@qq.com>
2020-09-03 18:17:09 +0800

    no message

--- pages/user-extract/index.js
+++ pages/user-extract/index.js
@@ -2,6 +2,9 @@
 
 Page({
   data: {
+    hidden: true, //提现弹窗
+    hiddenmx: true,//历史记录弹窗
+    countDownNum: '1'//倒计时初始值
   },
   onLoad: function () {
 
@@ -10,6 +13,47 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
+    
+  },
+  countDown: function () {
+    let that = this;
+    let countDownNum = that.data.countDownNum;//获取倒计时初始值
+    //如果将定时器设置在外面，那么用户就看不到countDownNum的数值动态变化，所以要把定时器存进data里面
+    that.setData({
+      timer: setInterval(function () {//这里把setInterval赋值给变量名为timer的变量
+        //每隔一秒countDownNum就减一，实现同步
+        countDownNum--;
+        //然后把countDownNum存进data，好让用户知道时间在倒计着
+        that.setData({
+          countDownNum: countDownNum
+        })
+        //在倒计时还未到0时，这中间可以做其他的事情，按项目需求来
+        if (countDownNum == 0) {
+          //这里特别要注意，计时器是始终一直在走的，如果你的时间为0，那么就要关掉定时器！不然相当耗性能
+          //因为timer是存在data里面的，所以在关掉时，也要在data里取出后再关闭
+          console.log(12)
+          that.setData({
+            hidden: !that.data.hidden
+          });
+          clearInterval(that.data.timer);
+          //关闭定时器之后，可作其他处理codes go here
+        }
+      }, 1000)
+    })
+  },
 
+  //提现按钮
+  show: function (e) {
+    this.countDown();
+    this.setData({
+      hidden: !this.data.hidden
+    })
+  },
+  //历史记录按钮
+  showmx: function (e) {
+    this.countDown();
+    this.setData({
+      hiddenmx: !this.data.hiddenmx
+    })
   },
 })
