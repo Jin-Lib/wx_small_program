1b096d15eae2b9c12ddf86b9e820c3d2df477593
LAPTOP-GQ2RS962\67433 <704714320@qq.com>
2020-09-04 17:32:15 +0800

    no message

--- pages/course-all/index.js
+++ pages/course-all/index.js
@@ -2,6 +2,42 @@
 
 Page({
   data: {
+    selected: true,
+    selected1: false,
+    selected2: false,
+    selected3: false,
+  },
+  selected: function (e) {
+    this.setData({
+      selected1: false,
+      selected: true,
+      selected2: false,
+      selected3: false,
+    });
+  },
+  selected1: function (e) {
+    this.setData({
+      selected: false,
+      selected1: true,
+      selected2: false,
+      selected3: false,
+    });
+  },
+  selected2: function (e) {
+    this.setData({
+      selected: false,
+      selected1: false,
+      selected2: true,
+      selected3: false,
+    });
+  },
+  selected3: function (e) {
+    this.setData({
+      selected: false,
+      selected1: false,
+      selected2: false,
+      selected3: true,
+    });
   },
   onLoad: function () {
 
