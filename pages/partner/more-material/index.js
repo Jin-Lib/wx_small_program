444b64e6b1fcbdf199de9817112d93d88fc6dc0d
LAPTOP-GQ2RS962\67433 <704714320@qq.com>
2020-09-10 23:11:23 +0800

    no message

--- pages/partner/more-material/index.js
+++ pages/partner/more-material/index.js
@@ -2,6 +2,13 @@
 
 Page({
   data: {
+    hiddenkg: true,//新老用户免拼规则弹窗
+  },
+  //新老用户免拼规则按钮
+  showkg: function (e) {
+    this.setData({
+      hiddenkg: !this.data.hiddenkg
+    })
   },
   onLoad: function () {
 
