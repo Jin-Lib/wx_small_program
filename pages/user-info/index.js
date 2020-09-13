181efbeb8a261db5a8ac27a4e46335ad0b1b783a
Jin <512364579@qq.com>
2020-09-13 15:29:15 +0800

    feat: 修改用户头像

--- pages/user-info/index.js
+++ pages/user-info/index.js
@@ -313,29 +313,25 @@ Page({
    * @returns {any}
    */
   uploadImg() {
+    const { infodetail } = this.data;
     var that = this;
     wx.chooseImage({  //从本地相册选择图片或使用相机拍照
       count: 1, // 默认9
       sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
       sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
       success:function(res){
-       //前台显示
-        that.setData({
-          source: res.tempFilePaths
-        })
+        //前台显示
+        infodetail.head_img = res.tempFilePaths[0]
         // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
-        var tempFilePaths = res.tempFilePaths
-         wx.uploadFile({
-          url: 'http://www.website.com/home/api/uploadimg',
-          filePath: tempFilePaths[0],
-          name: 'file',
-          success:function(res){
-            //打印
-            console.log(res.data)
-          }
+        that.setData({
+          infodetail
+        }, () => {
+          that.editUserInfo({
+            edit_key: 'HEAD',
+            edit_val: infodetail.head_img
+          });
         })
       }
-
     })
   }
 })
