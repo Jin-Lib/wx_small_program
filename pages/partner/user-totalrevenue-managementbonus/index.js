9d7c30ef5f2206f930de3c479abe4fcaaeced2de
LAPTOP-GQ2RS962\67433 <704714320@qq.com>
2020-09-13 15:18:27 +0800

    活动奖金"pages/partner/user-totalrevenue-activitybonus", 培育奖金"pages/partner/user-totalrevenue-cultivationbonus", 直邀奖金"pages/partner/user-totalrevenue-directinvitationbonus", 间接奖金"pages/partner/user-totalrevenue-indirectbonus", 管理奖金"pages/partner/user-totalrevenue-managementbonus", 推课奖金"pages/partner/user-totalrevenue-tuitionbonus",

---
+++ pages/partner/user-totalrevenue-managementbonus/index.js
@@ -0,0 +1,15 @@
+//index.js
+
+Page({
+  data: {
+  },
+  onLoad: function () {
+
+  },
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+})
--- pages/partner/user-totalrevenue/index.js
+++ pages/partner/user-totalrevenue/index.js
@@ -3,6 +3,48 @@
 Page({
   data: {
   },
+
+  //推课奖金
+  tuitionbonus: function (event) {
+    wx.navigateTo({
+      url: '/pages/partner/user-totalrevenue-tuitionbonus/index'
+    })
+  },
+
+  //活动奖金
+  activitybonus: function (event) {
+    wx.navigateTo({
+      url: '/pages/partner/user-totalrevenue-activitybonus/index'
+    })
+  },
+
+  //直邀奖金
+  directinvitationbonus: function (event) {
+    wx.navigateTo({
+      url: '/pages/partner/user-totalrevenue-directinvitationbonus/index'
+    })
+  },
+
+  //间接奖金
+  indirectbonus: function (event) {
+    wx.navigateTo({
+      url: '/pages/partner/user-totalrevenue-indirectbonus/index'
+    })
+  },
+
+  //管理奖金
+  managementbonus: function (event) {
+    wx.navigateTo({
+      url: '/pages/partner/user-totalrevenue-managementbonus/index'
+    })
+  },
+
+  //培育奖金
+  cultivationbonus: function (event) {
+    wx.navigateTo({
+      url: '/pages/partner/user-totalrevenue-cultivationbonus/index'
+    })
+  },
   onLoad: function () {
 
   },
