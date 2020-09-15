f39cd9bbfd4c7de9ea84d59c8cc2d6a8710472a2
杨伟韦 <865562227@qq.com>
2020-09-15 23:16:47 +0800

    fix

--- pages/mall/index.js
+++ pages/mall/index.js
@@ -65,12 +65,12 @@ Page({
 
   initData: function() {
     // 获取系统状态栏信息
-    wx.getSystemInfo({
-      success: e => {
-        this.globalData.StatusBar = e.statusBarHeight;
-        this.globalData.CustomBar = e.platform == 'android' ? e.statusBarHeight + 50 : e.statusBarHeight + 45;
-      }
-    });
+    // wx.getSystemInfo({
+    //   success: e => {
+    //     this.globalData.StatusBar = e.statusBarHeight;
+    //     this.globalData.CustomBar = e.platform == 'android' ? e.statusBarHeight + 50 : e.statusBarHeight + 45;
+    //   }
+    // });
  
     this.getBannerData();
     this.getTypeData();
