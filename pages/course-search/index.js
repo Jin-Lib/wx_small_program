3b6608002950aa67cc7f6383915c5a709c6229d0
SC-201912052156\Administrator <zc>
2020-09-17 22:27:39 +0800

    no message

--- pages/course-search/index.js
+++ pages/course-search/index.js
@@ -1,10 +1,34 @@
 //index.js
 
+const app = getApp()
 Page({
   data: {
     linkToKeywords: {},
+    //默认值  默认显示左上角
+    navbarData: {
+      showCapsule: 1
+    },
+    statusBarHeight: app.globalData.statusBarHeight
+  },
+    //点击返回首页
+  findex: function() {
+    wx.navigateBack({
+      delta: 1   //默认值是1
+    })
   },
   onLoad: function () {
+    // 返回
+    // wx.reLaunch({
+    //   url: '../logs/logs'
+    // })
+    // 获取系统状态栏信息
+    wx.getSystemInfo({
+      success: e => {
+        this.globalData.StatusBar = e.statusBarHeight;
+        this.globalData.CustomBar = e.platform == 'android' ? e.statusBarHeight + 50 :                    e.statusBarHeight + 45;
+     }
+    });
+ 
     let that = this;
     const eventChannel = this.getOpenerEventChannel()
     eventChannel.on('acceptDataFromOpenerPage', function(data) {
