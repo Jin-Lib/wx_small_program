aef439144ed1033cae063ae4b33530d1d0957ad6
SC-201912052156\Administrator <zc>
2020-09-18 01:53:13 +0800

    no message

--- pages/partner/user-temporaryfans/index.js
+++ pages/partner/user-temporaryfans/index.js
@@ -2,6 +2,28 @@
 
 Page({
   data: {
+    selected: true,
+    selected1: false,
+  },
+  
+  selected: function (e) {
+    this.setData({
+      selected1: false,
+      selected: true,
+    });
+  },
+  selected1: function (e) {
+    this.setData({
+      selected: false,
+      selected1: true,
+    });
+  },
+  
+  //点击立即去推广
+  tuiguang: function(event) {
+    wx.navigateTo({
+      url: '../partner-mall/index'
+    })
   },
   onLoad: function () {
 
