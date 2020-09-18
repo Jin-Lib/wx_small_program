2d95de203dd7d719750e4c191c15b89a22b6ccfd
SC-201912052156\Administrator <zc>
2020-09-18 23:37:21 +0800

    no message

--- pages/partner/b-user-lockpowder/index.js
+++ pages/partner/b-user-lockpowder/index.js
@@ -2,6 +2,13 @@
 
 Page({
   data: {
+    hiddenrule: true,
+  },
+  //0元领取按钮
+  showgroup: function (e) {
+    this.setData({
+      hiddenrule: !this.data.hiddenrule
+    })
   },
   onLoad: function () {
 
