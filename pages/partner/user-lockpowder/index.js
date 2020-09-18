c2a85b03b90e1c6b86af73cb204ce983f215cd0b
SC-201912052156\Administrator <zc>
2020-09-18 22:13:51 +0800

    no message

--- pages/partner/user-lockpowder/index.js
+++ pages/partner/user-lockpowder/index.js
@@ -2,10 +2,30 @@
 
 Page({
   data: {
+    hiddenrule: true, //规则说明弹窗
+    hiddenshare: true, //分享弹窗
     selected: true,
     selected1: false,
   },
+  //分享出去别人看到的页面临时入口
+  hhsr: function (event) {
+    wx.navigateTo({
+      url: '/pages/partner/b-user-lockpowder/index'
+    })
+  },
   
+  //规则说明按钮
+  showrule: function (e) {
+    this.setData({
+      hiddenrule: !this.data.hiddenrule
+    })
+  },
+  //分享按钮
+  showshare: function (e) {
+    this.setData({
+      hiddenshare: !this.data.hiddenshare
+    })
+  },
   selected: function (e) {
     this.setData({
       selected1: false,
